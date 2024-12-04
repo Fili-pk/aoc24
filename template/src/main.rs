e55e403c4d05b9e365129d131326d86e7fe232d3
fili_pk <fkwilczek@gmail.com>
2024-12-04 07:59:47 +0100

    Template update

--- template/src/main.rs
+++ template/src/main.rs
@@ -6,7 +6,90 @@ use std::{
 
 fn main() {
 	let file = BufReader::new(File::open("input").unwrap());
-	file.lines().map(|x| {
-		let x = x.unwrap();
-	})
+	let lines: Vec<_> = file
+		.lines()
+		.map(|x| {
+			let x = x.unwrap();
+		})
+		.collect();
+}
+
+struct FindOverlapping<'a, T> {
+	t: &'a [T],
+	pattern: &'a [T],
+	idx: usize,
+}
+
+impl<'a, T: Eq> Iterator for FindOverlapping<'a, T> {
+	type Item = usize;
+	fn next(&mut self) -> Option<usize> {
+		if self.idx + self.pattern.len().max(1) > self.t.len() {
+			return None;
+		}
+
+		while !self.t[self.idx..].starts_with(self.pattern) {
+			self.idx += 1;
+			if self.idx + self.pattern.len().max(1) > self.t.len() {
+				return None;
+			}
+		}
+
+		let v = Some(self.idx);
+		self.idx += 1;
+		v
+	}
+}
+
+trait FindOverlappingExt<T> {
+	fn find_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindOverlapping<'a, T>;
+}
+
+impl<T: Eq> FindOverlappingExt<T> for [T] {
+	fn find_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindOverlapping<'a, T> {
+		FindOverlapping {
+			t: self,
+			pattern,
+			idx: 0,
+		}
+	}
+}
+
+struct FindNotOverlapping<'a, T> {
+	t: &'a [T],
+	pattern: &'a [T],
+	idx: usize,
+}
+
+impl<'a, T: Eq> Iterator for FindNotOverlapping<'a, T> {
+	type Item = usize;
+	fn next(&mut self) -> Option<usize> {
+		if self.idx + self.pattern.len().max(1) > self.t.len() {
+			return None;
+		}
+
+		while !self.t[self.idx..].starts_with(self.pattern) {
+			self.idx += 1;
+			if self.idx + self.pattern.len().max(1) > self.t.len() {
+				return None;
+			}
+		}
+
+		let v = Some(self.idx);
+		self.idx += self.pattern.len();
+		v
+	}
+}
+
+trait FindNotOverlappingExt<T> {
+	fn find_not_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindNotOverlapping<'a, T>;
+}
+
+impl<T: Eq> FindNotOverlappingExt<T> for [T] {
+	fn find_not_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindNotOverlapping<'a, T> {
+		FindNotOverlapping {
+			t: self,
+			pattern,
+			idx: 0,
+		}
+	}
 }
