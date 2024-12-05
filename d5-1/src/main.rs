74cf9847674f9da5909e86a6248553111ba11110
fili_pk <fkwilczek@gmail.com>
2024-12-05 06:57:23 +0100

    Day 5 (This is awful but works :))

---
+++ d5-1/src/main.rs
@@ -0,0 +1,127 @@
+use itertools::Itertools;
+use std::{
+	collections::HashMap,
+	fs::File,
+	io::{BufRead, BufReader},
+};
+
+fn main() {
+	let file = BufReader::new(File::open("input").unwrap());
+	let l: Vec<_> = file.lines().map(|x| x.unwrap()).collect();
+
+	let mut l1: HashMap<i32, Vec<i32>> = HashMap::new();
+	l.iter().take_while(|x| !x.is_empty()).for_each(|x| {
+		let a = x.split_once('|').unwrap();
+		l1.entry(a.1.parse().unwrap())
+			.and_modify(|x| x.push(a.0.parse().unwrap()))
+			.or_insert(vec![a.0.parse().unwrap()]);
+	});
+
+	let l2: Vec<Vec<i32>> = l
+		.iter()
+		.filter(|x| !x.contains('|') && !x.is_empty())
+		.map(|x| x.split(',').map(|x| x.parse().unwrap()).collect())
+		.collect();
+
+	let mut s = 0;
+	'w: for a in l2 {
+		'q: for (i, b) in a.iter().enumerate() {
+			for c in l1.get(b).unwrap_or(&vec![]).iter() {
+				if !a.contains(c) {
+					continue;
+				}
+
+				println!("{a:?} {i}");
+				for q in a.iter().skip(i) {
+					if q == c {
+						continue 'w;
+					}
+				}
+			}
+		}
+
+		s += a[a.len() / 2];
+	}
+
+	println!("{s}")
+}
+
+struct FindOverlapping<'a, T> {
+	t: &'a [T],
+	pattern: &'a [T],
+	idx: usize,
+}
+
+impl<'a, T: Eq> Iterator for FindOverlapping<'a, T> {
+	type Item = usize;
+	fn next(&mut self) -> Option<usize> {
+		if self.idx + self.pattern.len().max(1) > self.t.len() {
+			return None;
+		}
+
+		while !self.t[self.idx..].starts_with(self.pattern) {
+			self.idx += 1;
+			if self.idx + self.pattern.len().max(1) > self.t.len() {
+				return None;
+			}
+		}
+
+		let v = Some(self.idx);
+		self.idx += 1;
+		v
+	}
+}
+
+trait FindOverlappingExt<T> {
+	fn find_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindOverlapping<'a, T>;
+}
+
+impl<T: Eq> FindOverlappingExt<T> for [T] {
+	fn find_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindOverlapping<'a, T> {
+		FindOverlapping {
+			t: self,
+			pattern,
+			idx: 0,
+		}
+	}
+}
+
+struct FindNotOverlapping<'a, T> {
+	t: &'a [T],
+	pattern: &'a [T],
+	idx: usize,
+}
+
+impl<'a, T: Eq> Iterator for FindNotOverlapping<'a, T> {
+	type Item = usize;
+	fn next(&mut self) -> Option<usize> {
+		if self.idx + self.pattern.len().max(1) > self.t.len() {
+			return None;
+		}
+
+		while !self.t[self.idx..].starts_with(self.pattern) {
+			self.idx += 1;
+			if self.idx + self.pattern.len().max(1) > self.t.len() {
+				return None;
+			}
+		}
+
+		let v = Some(self.idx);
+		self.idx += self.pattern.len();
+		v
+	}
+}
+
+trait FindNotOverlappingExt<T> {
+	fn find_not_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindNotOverlapping<'a, T>;
+}
+
+impl<T: Eq> FindNotOverlappingExt<T> for [T] {
+	fn find_not_overlapping<'a>(&'a self, pattern: &'a [T]) -> FindNotOverlapping<'a, T> {
+		FindNotOverlapping {
+			t: self,
+			pattern,
+			idx: 0,
+		}
+	}
+}
