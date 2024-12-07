9f92d5e7fb0bc15ed8188b3f6b8b4426e40e2999
fili_pk <fkwilczek@gmail.com>
2024-12-07 11:10:09 +0100

    Updated template

--- template/src/main.rs
+++ template/src/main.rs
@@ -2,6 +2,7 @@ use itertools::Itertools;
 use std::{
 	fs::File,
 	io::{BufRead, BufReader},
+	mem::{transmute_copy, ManuallyDrop, MaybeUninit},
 };
 
 fn main() {
@@ -12,6 +13,63 @@ fn main() {
 			let x = x.unwrap();
 		})
 		.collect();
+}
+
+struct CProduct<'a, T, const SIZE: usize, const OUT_SIZE: usize> {
+	src: &'a [T; SIZE],
+	idx: [usize; OUT_SIZE],
+	end: bool,
+}
+
+trait CProductExt<T, const SIZE: usize> {
+	fn c_product<const OUT_SIZE: usize>(&self) -> CProduct<T, SIZE, OUT_SIZE>;
+}
+
+impl<T: Clone, const SIZE: usize> CProductExt<T, SIZE> for [T; SIZE] {
+	fn c_product<const OUT_SIZE: usize>(&self) -> CProduct<T, SIZE, OUT_SIZE> {
+		CProduct {
+			src: self,
+			idx: [0; OUT_SIZE],
+			end: SIZE == 0 || OUT_SIZE == 0,
+		}
+	}
+}
+
+impl<'a, T: Clone, const SIZE: usize, const OUT_SIZE: usize> Iterator
+	for CProduct<'a, T, SIZE, OUT_SIZE>
+{
+	type Item = [T; OUT_SIZE];
+	fn next(&mut self) -> Option<[T; OUT_SIZE]> {
+		if self.end {
+			return None;
+		}
+
+		let mut res = [const { MaybeUninit::uninit() }; OUT_SIZE];
+		let mut i = 0;
+		while i < OUT_SIZE {
+			res[i] = MaybeUninit::new(self.src[self.idx[i]].clone());
+			if self.idx[i] != SIZE - 1 {
+				self.idx[i] += 1;
+				for i in 0..i {
+					self.idx[i] = 0;
+				}
+				break;
+			}
+
+			i += 1;
+		}
+
+		if i == OUT_SIZE {
+			self.end = true;
+			return Some(unsafe { transmute_copy(&ManuallyDrop::new(res)) });
+		}
+
+		for i in (i + 1)..OUT_SIZE {
+			res[i] = MaybeUninit::new(self.src[self.idx[i]].clone());
+		}
+
+		Some(unsafe { transmute_copy(&ManuallyDrop::new(res)) })
+	}
 }
 
 struct FindOverlapping<'a, T> {
